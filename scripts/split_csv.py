16bd8810829495bbc122b772c67e417c054df60f
Monika Burnejko <moniburnejko@gmail.com>
2026-01-31 19:45:24 +0100

    chore: restructure project folders and standardize naming

--- scripts/split_csv.py
+++ scripts/split_csv.py
@@ -8,14 +8,12 @@ trans_cols = ['trans_num', 'trans_date_trans_time', 'cc_num', 'amt', 'merchant',
 client_cols = ['cc_num', 'first', 'last', 'gender', 'street', 'city', 'state', 'zip', 'job', 'dob']
 merchant_cols = ['trans_num', 'merchant', 'merch_lat', 'merch_long']
 
-# transaction table
 transaction = df[trans_cols].drop_duplicates()
-# client table
 client = df[client_cols].drop_duplicates()
-# merch table
 merchant = df[merchant_cols].drop_duplicates()
 
-csv_trans = transaction.to_csv('transaction.csv', index=False)
-csv_client = client.to_csv('client.csv', index=False)
-csv_merc = merchant.to_csv('merchant.csv', index=False)
-print("Data split into transaction, client, and merchant tables.")
+trans_csv = transaction.to_csv('transaction.csv', index=False)
+client_csv = client.to_csv('client.csv', index=False)
+merchant_csv = merchant.to_csv('merchant.csv', index=False)
+
+print("Done! Data split into transaction, client, and merchant csv files.")

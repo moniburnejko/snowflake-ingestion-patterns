cd16d6406c1c219b4ea2d1994f0099f4e2c1a417
Monika Burnejko <moniburnejko@gmail.com>
2026-01-31 19:17:09 +0100

    feat: add terraform RBAC config for snowflake

---
+++ terraform/main.tf
@@ -0,0 +1,201 @@
+# RBAC
+
+# CREATE CUSTOM ROLES 
+# create all roles defined in variable custom_role
+resource "snowflake_account_role" "roles" {
+  provider = snowflake.security
+  for_each = toset(var.custom_role)
+  name     = each.value
+}
+
+# ROLE HIERARCHY (grant to SYSADMIN)
+resource "snowflake_grant_account_role" "sysadmin_grants" {
+  provider         = snowflake.sys
+  for_each         = toset(var.custom_role)
+  role_name        = snowflake_account_role.roles[each.value].name
+  parent_role_name = "SYSADMIN"
+}
+
+# CONFIG MAPS AND HELPERS
+locals {
+  # helpers for common grants loops
+  warehouse_grants = setproduct(var.warehouses, var.custom_role)
+  schema_grants    = setproduct(var.schemas, var.custom_role)
+
+  # SCHEMA CREATION PRIVILEGES (role -> list of privileges)
+  schema_creation_map = {
+    PIPEADMIN  = ["CREATE PIPE", "CREATE STREAM"]
+    TASKADMIN  = ["CREATE TASK"]
+    ALERTADMIN = ["CREATE ALERT"]
+  }
+
+  # PIPEADMIN OBJECT PRIVILEGES (object type -> list of privileges)
+  pipeadmin_objects = {
+    STAGES         = ["USAGE"]
+    "FILE FORMATS" = ["USAGE"]
+    TABLES         = ["INSERT", "SELECT"]
+    PIPES          = ["MONITOR"]
+  }
+
+  # TASKADMIN OBJECT PRIVILEGES (object type -> list of privileges)
+  # objects on ALL schemas (general)
+  taskadmin_general_objects = {
+    TASKS            = ["MONITOR"]
+    "DYNAMIC TABLES" = ["SELECT", "OPERATE"]
+  }
+  # objects on RW schemas (read-write)
+  taskadmin_rw_objects = {
+    TABLES  = ["SELECT", "INSERT", "UPDATE", "DELETE", "TRUNCATE"]
+    STREAMS = ["SELECT"]
+  }
+  # objects on RO schemas (read-only)
+  taskadmin_ro_objects = {
+    TABLES = ["SELECT"]
+  }
+}
+
+
+# COMMON GRANTS (USAGE)
+
+# WAREHOUSE USAGE
+resource "snowflake_grant_privileges_to_account_role" "warehouse_usage" {
+  for_each   = { for pair in local.warehouse_grants : "${pair[0]}_${pair[1]}" => { wh = pair[0], role = pair[1] } }
+  account_role_name  = snowflake_account_role.roles[each.value.role].name
+  privileges = ["USAGE"]
+  on_account_object {
+    object_type = "WAREHOUSE"
+    object_name = each.value.wh
+  }
+}
+
+# DATABASE USAGE
+resource "snowflake_grant_privileges_to_account_role" "database_usage" {
+  for_each   = toset(var.custom_role)
+  account_role_name  = snowflake_account_role.roles[each.value].name
+  privileges = ["USAGE"]
+  on_account_object {
+    object_type = "DATABASE"
+    object_name = var.database
+  }
+}
+
+# SCHEMA USAGE
+resource "snowflake_grant_privileges_to_account_role" "schema_usage" {
+  for_each   = { for pair in local.schema_grants : "${pair[0]}_${pair[1]}" => { schema = pair[0], role = pair[1] } }
+  account_role_name  = snowflake_account_role.roles[each.value.role].name
+  privileges = ["USAGE"]
+  on_schema {
+    schema_name = "\"${var.database}\".\"${each.value.schema}\""
+  }
+}
+
+# SCHEMA CREATION (for all roles defined in map)
+resource "snowflake_grant_privileges_to_account_role" "schema_creation" {
+  for_each = { 
+    for pair in setproduct(var.schemas, keys(local.schema_creation_map)) : 
+    "${pair[0]}_${pair[1]}" => { schema = pair[0], role = pair[1] } 
+  }
+  account_role_name = snowflake_account_role.roles[each.value.role].name
+  privileges        = local.schema_creation_map[each.value.role]
+  on_schema {
+    schema_name = "\"${var.database}\".\"${each.value.schema}\""
+  }
+}
+
+
+# PIPEADMIN OBJECT GRANTS
+
+resource "snowflake_grant_privileges_to_account_role" "pipeadmin_objects_future" {
+  for_each = { 
+    for pair in setproduct(var.schemas, keys(local.pipeadmin_objects)) : 
+    "${pair[0]}_${pair[1]}" => { schema = pair[0], type = pair[1] } 
+  }
+  account_role_name = snowflake_account_role.roles["PIPEADMIN"].name
+  privileges        = local.pipeadmin_objects[each.value.type]
+  on_schema_object {
+    future {
+      object_type_plural = each.value.type
+      in_schema          = "\"${var.database}\".\"${each.value.schema}\""
+    }
+  }
+}
+
+resource "snowflake_grant_privileges_to_account_role" "pipeadmin_objects_all" {
+  for_each = { 
+    for pair in setproduct(var.schemas, keys(local.pipeadmin_objects)) : 
+    "${pair[0]}_${pair[1]}" => { schema = pair[0], type = pair[1] } 
+  }
+  account_role_name = snowflake_account_role.roles["PIPEADMIN"].name
+  privileges        = local.pipeadmin_objects[each.value.type]
+  on_schema_object {
+    all {
+      object_type_plural = each.value.type
+      in_schema          = "\"${var.database}\".\"${each.value.schema}\""
+    }
+  }
+}
+
+
+# TASKADMIN GRANTS
+
+resource "snowflake_grant_privileges_to_account_role" "taskadmin_account_grants" {
+  account_role_name  = snowflake_account_role.roles["TASKADMIN"].name
+  privileges = ["EXECUTE TASK", "EXECUTE MANAGED TASK"]
+  on_account = true
+  provider   = snowflake.account
+}
+
+# helper resource for taskadmin object grants (RW, RO, General)
+# create separate resources for future and all grants
+
+resource "snowflake_grant_privileges_to_account_role" "taskadmin_objects_future" {
+  for_each = merge(
+    { for pair in setproduct(var.schemas_rw, keys(local.taskadmin_rw_objects)) : "RW_${pair[0]}_${pair[1]}" => { schema = pair[0], type = pair[1], privs = local.taskadmin_rw_objects[pair[1]] } },
+    { for pair in setproduct(var.schemas_ro, keys(local.taskadmin_ro_objects)) : "RO_${pair[0]}_${pair[1]}" => { schema = pair[0], type = pair[1], privs = local.taskadmin_ro_objects[pair[1]] } },
+    { for pair in setproduct(var.schemas, keys(local.taskadmin_general_objects)) : "GEN_${pair[0]}_${pair[1]}" => { schema = pair[0], type = pair[1], privs = local.taskadmin_general_objects[pair[1]] } }
+  )
+  account_role_name = snowflake_account_role.roles["TASKADMIN"].name
+  privileges        = each.value.privs
+  on_schema_object {
+    future {
+      object_type_plural = each.value.type
+      in_schema          = "\"${var.database}\".\"${each.value.schema}\""
+    }
+  }
+}
+
+resource "snowflake_grant_privileges_to_account_role" "taskadmin_objects_all" {
+  for_each = merge(
+    { for pair in setproduct(var.schemas_rw, keys(local.taskadmin_rw_objects)) : "RW_${pair[0]}_${pair[1]}" => { schema = pair[0], type = pair[1], privs = local.taskadmin_rw_objects[pair[1]] } },
+    { for pair in setproduct(var.schemas_ro, keys(local.taskadmin_ro_objects)) : "RO_${pair[0]}_${pair[1]}" => { schema = pair[0], type = pair[1], privs = local.taskadmin_ro_objects[pair[1]] } },
+    { for pair in setproduct(var.schemas, keys(local.taskadmin_general_objects)) : "GEN_${pair[0]}_${pair[1]}" => { schema = pair[0], type = pair[1], privs = local.taskadmin_general_objects[pair[1]] } }
+  )
+  account_role_name = snowflake_account_role.roles["TASKADMIN"].name
+  privileges        = each.value.privs
+  on_schema_object {
+    all {
+      object_type_plural = each.value.type
+      in_schema          = "\"${var.database}\".\"${each.value.schema}\""
+    }
+  }
+}
+
+
+# ALERTADMIN GRANTS
+
+resource "snowflake_grant_privileges_to_account_role" "alertadmin_account_grants" {
+  account_role_name  = snowflake_account_role.roles["ALERTADMIN"].name
+  privileges = ["EXECUTE ALERT", "EXECUTE MANAGED ALERT"]
+  on_account = true
+  provider   = snowflake.account
+}
+
+# database monitor
+resource "snowflake_grant_privileges_to_account_role" "alertadmin_db_monitor" {
+  account_role_name  = snowflake_account_role.roles["ALERTADMIN"].name
+  privileges = ["MONITOR"]
+  on_account_object {
+    object_type = "DATABASE"
+    object_name = var.database
+  }
+}

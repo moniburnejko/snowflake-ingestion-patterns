ba2b93a77660b310da1e5a6366f5afa3be0429fe
Monika Burnejko <moniburnejko@gmail.com>
2026-01-31 13:06:09 +0100

    chore: correct alert names and comments

--- sql/alerts.sql
+++ sql/alerts.sql
@@ -5,21 +5,23 @@ USE ROLE alertadmin;
 USE WAREHOUSE poc_wh;
 USE DATABASE poc_db;
 USE SCHEMA poc2_schema; 
+ALTER SESSION SET TIMEZONE = 'Europe/Warsaw';
 
 -- SNOWPIPE ERROR ALERT --
-CREATE ALERT IF NOT EXISTS alert_poc2_snowpipe_errors
+CREATE ALERT IF NOT EXISTS alert_snowpipe_errors
   WAREHOUSE = poc_wh
   SCHEDULE = '15 MINUTE'
 IF (EXISTS (
   SELECT 1
   FROM TABLE(INFORMATION_SCHEMA.COPY_HISTORY(
     TABLE_NAME => 'POC2_LANDING',
-    -- check only records since last successful alert execution
+    -- check only records since last successful alert run
     START_TIME => SNOWFLAKE.ALERT.LAST_SUCCESSFUL_SCHEDULED_TIME()
   ))
   WHERE 
     pipe_name = 'POC2_PIPE' 
     AND error_count > 0
+    AND table_name = 'POC2_LANDING'
     AND (last_load_time BETWEEN SNOWFLAKE.ALERT.LAST_SUCCESSFUL_SCHEDULED_TIME() 
       AND SNOWFLAKE.ALERT.SCHEDULED_TIME())
 )) 
@@ -32,7 +34,7 @@ THEN
 
 
 -- TASK ERROR ALERT --
-CREATE ALERT IF NOT EXISTS alert_poc2_task_errors
+CREATE ALERT IF NOT EXISTS alert_task_errors
   WAREHOUSE = poc_wh
   SCHEDULE = '15 MINUTE'
 IF (EXISTS (
@@ -52,14 +54,14 @@ THEN
 );
 
 -- when created, alerts are in SUSPENDED state, resume them to enable monitoring
-ALTER ALERT alert_poc2_task_errors RESUME;
-ALTER ALERT alert_poc2_snowpipe_errors RESUME;
+//ALTER ALERT alert_task_errors RESUME;
+//ALTER ALERT alert_snowpipe_errors RESUME;
+ALTER ALERT alert_task_errors SUSPEND;
+ALTER ALERT alert_snowpipe_errors SUSPEND;
 
-//ALTER ALERT alert_poc2_task_errors SUSPEND;
-//ALTER ALERT alert_poc2_snowpipe_errors SUSPEND;
+-- check created alerts details
+SHOW ALERTS;
 
 -- execute alerts manually for testing
-EXECUTE ALERT alert_poc2_snowpipe_errors;
-EXECUTE ALERT alert_poc2_task_errors;
-
-SHOW ALERTS;
+//EXECUTE ALERT alert_snowpipe_errors;
+//EXECUTE ALERT alert_task_errors;

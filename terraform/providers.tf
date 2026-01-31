a01410eb96d6c5cdddb0e756425a54b6762f1a50
Monika Burnejko <moniburnejko@gmail.com>
2026-01-31 20:30:29 +0100

    feat: add AWS provider config

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,10 +1,21 @@
 terraform {
   required_providers {
+    
+        aws = {
+      source = "hashicorp/aws"
+      version = "6.30.0"
+    }
     snowflake = {
       source = "snowflakedb/snowflake"
       version = "2.12.0"
     }
   }
+}
+
+provider "aws" {
+  region     = "eu-north-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 # default provider = securityadmin

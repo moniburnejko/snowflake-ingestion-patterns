85179686522331aad325f02f15ee22e9ad305afd
moniburnejko <moniburnejko@gmail.com>
2026-02-02 23:01:32 +0100

    feat: add snapshot for clients to track changes over time (scd2)

---
+++ dbt_fun/snapshots/clients_snapshot.sql
@@ -0,0 +1,12 @@
+{% snapshot clients_snapshot %}
+
+{{ config(
+    unique_key='card_num',
+    strategy='timestamp',
+    updated_at='meta_load_ts'
+) }}
+
+select *
+from {{ ref('int_clients_deduped') }}
+
+{% endsnapshot %}
